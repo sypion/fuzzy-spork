6435a42fb2c76a354f469f4a6395caec50219864
Syphon <sypion@users.noreply.github.com>
2023-08-30 15:08:45 -0700

    feat: play the game by refreshing the page

--- main.js
+++ main.js
@@ -1,7 +1,43 @@
 function getComputerChoice() {
-    let choice = ["Rock", "Paper", "Scissors"]
-    return choice[Math.floor(Math.random() * choice.length)];
+    let choices = ["Rock", "Paper", "Scissors"];
+
+    return choices[Math.floor(Math.random() * choices.length)];
 }
 
+function getPlayerChoice() {
+    let choices = ["Rock", "Paper", "Scissors"];
 
-console.log(getComputerChoice());
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+computerSelection = getComputerChoice();
+playerSelection = getPlayerChoice();
+
+let playerScore = 0
+let computerScore = 0
+
+function playRound(playerSelection, computerSelection) {
+
+    if (
+		(playerSelection === "Rock" && computerSelection === "Scissors") ||
+		(playerSelection === "Paper" && computerSelection === "Rock") ||
+		(playerSelection === "Scissors" && computerSelection === "Paper")
+	) {
+		playerScore += 1
+        console.log("You chose " + playerSelection + " and the computer chose " + computerSelection + ". You win this round!")
+    } else if (
+        (computerSelection === "Rock" && playerSelection === "Scissors") ||
+		(computerSelection === "Paper" && playerSelection === "Rock") ||
+		(computerSelection === "Scissors" && playerSelection === "Paper")
+    ) {
+        computerScore += 1
+        console.log("You chose " + playerSelection + " and the computer chose " + computerSelection + ". The computer wins this round!")
+    } else if (
+        (playerSelection === computerSelection)
+    ) {
+        console.log("Looks like it\'s a tie, as you both chose " + playerSelection + "!")
+    }
+
+};
+
+console.log(playRound(playerSelection, computerSelection));
