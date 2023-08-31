a2cfd37fa8d85d24f022cc04226a005eb890cd06
Syphon <sypion@users.noreply.github.com>
2023-08-30 19:16:12 -0700

    complete initial js for game

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
+// Understand that these are the only options possible
+
+const ROCK = 'rock';
+const PAPER = 'paper';
+const SCISSORS = 'scissors';
+
+
 // Let the computer choose first
 function getComputerChoice() {
     let choices = ["rock", "paper", "scissors"];
-
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
 // Prompt the player for their choice
 function getPlayerChoice() {
     let playerSelection;
-    playerSelection = prompt("What's your move: rock, paper or scissors?")
-    playerSelection = playerSelection.toLowerCase(); // always output the user's answer lowercase
+    let validInput = false;
+    while (!validInput) {
+        playerSelection = prompt("What's your move: rock, paper or scissors?")
+        playerSelection = playerSelection.toLowerCase(); // always output the user's answer lowercase
+
+        // Ensure that what was entered is 
+        if (playerSelection === ROCK || playerSelection === PAPER || playerSelection === SCISSORS) {
+            validInput = true;
+        }
+    }
     return playerSelection;
 }
 
@@ -27,30 +41,39 @@ let computerScore = 0
 
 function playRound(playerSelection, computerSelection) {
 
+    // Player wins
+
     if (
 		(playerSelection === "rock" && computerSelection === "scissors") ||
 		(playerSelection === "paper" && computerSelection === "rock") ||
 		(playerSelection === "scissors" && computerSelection === "paper")
 	) {
-		playerScore += 1
-        console.log("You chose " + playerSelection + " and the computer chose " + computerSelection + ". You win this round!")
-        console.log("The score is now " + playerScore +"! Onwards and upwards.")
+		playerScore += 1;
+        console.log(`You chose ${playerSelection} and the computer chose ${computerSelection} You win this round!`);
+        console.log("The score is now " + playerScore +"! Onwards and upwards.");
+
+    // Computer wins
+
     } else if (
         (computerSelection === "rock" && playerSelection === "scissors") ||
 		(computerSelection === "paper" && playerSelection === "rock") ||
 		(computerSelection === "scissors" && playerSelection === "paper")
     ) {
         computerScore += 1
-        console.log("You chose " + playerSelection + " and the computer chose " + computerSelection + ". The computer wins this round!")
-        console.log("The score is now " + playerScore +"! Keep going.")
+        console.log(`You chose ${playerSelection} and the computer chose ${computerSelection}. The computer wins this round!`)
+        console.log(`Your score is ${playerScore}! Keep going.`)
+
+    // Tie
+
     } else if (
         (playerSelection === computerSelection)
     ) {
-        console.log("Looks like it\'s a tie, as you both chose " + playerSelection + "!")
-        console.log("The score is now " + playerScore +"!")
+        console.log(`Looks like it\'s a tie, as you both chose ${playerSelection}!`)
+        console.log(`The score is ${playerScore}!`)
     }
 
 };
+
 
 // Output result in console
 
